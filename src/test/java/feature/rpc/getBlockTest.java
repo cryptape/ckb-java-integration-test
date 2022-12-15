6928e6370dbbaed1468fa72a968e8d4fcb31f2f6
xueyanli <xueyanli19950914@gmail.com>
2022-12-15 16:11:41 +0800

    add demo case

--- src/test/java/feature/rpc/getBlockTest.java
+++ src/test/java/feature/rpc/getBlockTest.java
@@ -16,20 +16,20 @@ import static com.ckb.utils.HttpUtils.post;
 
 public class getBlockTest extends BeforeSuite {
     public static org.apache.log4j.Logger logger = Logger.getLogger(getBlockTest.class);
-    Map<String, Object> requestBody = getBodyForJsonRpc();
     public static final String url = CKB_MAINNET;
     @Test(dataProvider = "get_block")
     @Description("get_block(block_hash, verbosity, with_cycles)")
-    public void TestGetBlock(){
-        logger.info("get_block...");
-        Response response = post(url, getHeaders(), requestBody);
+    public void TestGetBlock(Map<String, Object> body){
+        logger.info("begin...");
+        Response response = post(url, getHeaders(), body);
         Assert.assertEquals(200, response.getStatusCode());
         System.out.println("result:" + getValueByJsonPath(response, "result"));
     }
 
     @DataProvider(name = "get_block")
     public Object[][] body(){
-        requestBody.put("method", "send_alert");
+        Map<String, Object> requestBody = getBodyForJsonRpc();
+        requestBody.put("method", "get_block");
         ArrayList<String> params = new ArrayList<>();
         params.add("0xa5f5c85987a15de25661e5a214f2c1449cd803f071acc7999820f25246471f40");
         requestBody.put("params", params);
